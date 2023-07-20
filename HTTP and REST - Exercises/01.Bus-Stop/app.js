f4e0464216dbbf10cb099bae14698de93a6b78bf
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-07-20 19:59:13 +0100

    Update app.js

--- HTTP and REST - Exercises/01.Bus-Stop/app.js
+++ HTTP and REST - Exercises/01.Bus-Stop/app.js
@@ -1,27 +1,27 @@
-async function getInfo() {
-  const busStopID = document.querySelector("#stopId").value;
-  const stopName = document.querySelector("#stopName");
-  const list = document.querySelector("#buses");
-  try {
-    const res = await fetch(
-      `http://localhost:3030/jsonstore/bus/businfo/${busStopID}`
-    );
-    if (res.ok === false) {
-      throw new Error(`${res.status}${res.statusText}`);
-    }
-    const data = await res.json();
-    list.innerHTML = "";
-
-    stopName.textContent = data.name;
-
-    const buses = data.buses;
-
-    Object.entries(buses).forEach(([busId, time]) => {
-      const li = document.createElement("li");
-      li.textContent = `Bus ${busId} arrives in ${time} minutes`;
-      list.appendChild(li);
+function getInfo() {
+  let baseUrl = "http://localhost:3030/jsonstore/bus/businfo/";
+  let inputEl = document.getElementById("stopId");
+  let ulEl = document.getElementById("buses");
+  let divEl = document.getElementById("stopName");
+  fetch(`${baseUrl}/${inputEl.value}`)
+    .then((response) => response.json())
+    .catch((error) => {
+      divEl.textContent = "Error";
+      ulEl.innerHTML = "";
+    })
+    .then((data) => {
+      let buses = data.buses;
+      let name = data.name;
+      divEl.textContent = name;
+      ulEl.innerHTML = '';
+      Object.keys(buses).forEach((bus) => {
+        let liEl = document.createElement("li");
+        liEl.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+        ulEl.appendChild(liEl);
+      })
+    })
+    .catch((error) => {
+      divEl.textContent = "Error";
+      ulEl.innerHTML = "";
     });
-  } catch (error) {
-    stopName.textContent = "Error";
-  }
 }
